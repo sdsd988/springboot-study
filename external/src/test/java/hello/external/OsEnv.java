5e17f3a09ffab69ef7bc095f6639b2a8a79c3e21
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-19 17:56:24 +0900

    외부설정과 프로필 1-1

---
+++ external/src/test/java/hello/external/OsEnv.java
@@ -0,0 +1,18 @@
+package hello.external;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+@Slf4j
+public class OsEnv {
+
+    public static void main(String[] args) {
+
+        Map<String, String> getenv = System.getenv();
+        for (String key : getenv.keySet()) {
+            log.info("env {} = {}", key, System.getenv(key));
+        }
+    }
+}
