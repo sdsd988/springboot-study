4b8906a263082c42358248fbca9c0b5890259983
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-22 18:53:39 +0900

    스프링부트 끝

--- actuator/src/main/java/hello/ActuatorApplication.java
+++ actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,5 +1,7 @@
 package hello;
 
+import hello.order.guage.StockConfigV1;
+import hello.order.guage.StockConfigV2;
 import hello.order.v0.OrderConfigV0;
 import hello.order.v1.OrderConfigV1;
 import hello.order.v2.OrderConfigV2;
@@ -15,7 +17,7 @@ import org.springframework.context.annotation.Import;
 //@Import(OrderConfigV1.class) // 한번은 호출해야 메트릭으로 등록
 //@Import(OrderConfigV2.class)
 //@Import(OrderConfigV3.class)
-@Import(OrderConfigV4.class)
+@Import({OrderConfigV4.class, StockConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
