7cddf7066d4b85ba12c7f48291558777ae584ed8
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-22 18:12:04 +0900

    custom metrics timer

--- actuator/src/main/java/hello/ActuatorApplication.java
+++ actuator/src/main/java/hello/ActuatorApplication.java
@@ -2,6 +2,9 @@ package hello;
 
 import hello.order.v0.OrderConfigV0;
 import hello.order.v1.OrderConfigV1;
+import hello.order.v2.OrderConfigV2;
+import hello.order.v3.OrderConfigV3;
+import hello.order.v4.OrderConfigV4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +12,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import(OrderConfigV0.class)
-@Import(OrderConfigV1.class) // 한번은 호출해야 메트릭으로 등록
+//@Import(OrderConfigV1.class) // 한번은 호출해야 메트릭으로 등록
+//@Import(OrderConfigV2.class)
+//@Import(OrderConfigV3.class)
+@Import(OrderConfigV4.class)
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
